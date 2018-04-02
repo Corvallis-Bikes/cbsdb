b83a5d3f79b054841a1fb5c9ec525fa767e3f08c
rickD <debboutr@gmail.com>
2018-04-02 07:35:53 -0700

    migrate

--- bikes/views.py
+++ bikes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request, template_name='index.html'):
+    form = BikeForm(request.POST or None)
+    if request.method == 'POST':
+        form = BikeForm(request.POST, request.FILES,
+                            user    = request.user,
+                            initial = initial_values)
+        if form.is_valid():
+            pass
+
+    return render(request, template_name, {'form': form})
